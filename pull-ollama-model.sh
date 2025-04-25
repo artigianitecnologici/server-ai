eff2729501126f6805034715b0cde5510298362b
Fabio Ferrarini <ferrarini09@gmail.com>
2025-04-25 09:47:59 +0200

    Create pull-ollama-model.sh

---
+++ pull-ollama-model.sh
@@ -0,0 +1,8 @@
+ollama pull deepseek-r1:14b                       
+ollama pull gemma3:12b-it-qat                 
+ollama pull gemma:2b                            
+ollama pull llama3:latest                     
+ollama pull llama2-uncensored:7b-chat-q4_0    
+ollama pull llama3.1:8b                       
+ollama pull qwen2.5-coder:1.5b-base           
+ollama pull codellama:7b                      
